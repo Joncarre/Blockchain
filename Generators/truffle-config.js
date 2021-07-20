a392847b4cfdb934662f3cbd6950981379b1fdb2
Joncarre <joncarre@ucm.es>
2021-07-20 21:49:13 +0200

    full version pet-shop

--- Generators/truffle-config.js
+++ Generators/truffle-config.js
@@ -1,14 +1,16 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = "orange endorse control wrist surface borrow someone pact food view naive salute";
+const projectID = "9e26eee8ea4c498bbeab559b287115b6";
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://kovan.infura.io/v3/" + projectID);
+      },
+      network_id: 42,
+      gasPrice: 20000000000,
+      gas: 3716887
     },
-    develop: {
-      port: 8545
-    }
-  }
-};
+   },
+ };
